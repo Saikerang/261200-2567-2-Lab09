24b85c0bb88c5660efc24d58bd3414bddad238fe
Ambatunut <98247362+Saikerang@users.noreply.github.com>
2025-02-26 01:46:10 +0700

    Add files via upload

---
+++ DIP/EmailNotifier.java
@@ -0,0 +1,10 @@
+public class EmailNotifier implements NotificationService{
+    public EmailNotifier(){
+    }
+
+    @Override
+    public void notify(String message) {
+        System.out.println("Sending email with message: " + message);
+    }
+
+}
---
+++ DIP/SMSNotifier.java
@@ -0,0 +1,10 @@
+public class SMSNotifier implements NotificationService{
+    public SMSNotifier(){
+    }
+
+    @Override
+    public void notify(String message) {
+        System.out.println("Sending sms with message: " + message);
+    }
+
+}
